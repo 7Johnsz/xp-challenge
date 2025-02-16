047085afc32581795a247d1fe5119bac3d9feb03
7johnsz <contatoerro708@gmail.com>
2025-02-16 19:53:09 -0300

    feat: :sparkles: ping endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
+# from fastapi.middleware.gzip import GZipMiddleware
+from .controllers.routers.account import signup
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(docs="/docs")
+
+# app.add_middleware(GZipMiddleware)
+app.include_router(signup.router)
 
 @app.get("/")
 async def root():
     return "Hello World!"
+
+@app.get("/ping")
+async def ping():
+    return "Pong!"
