de316e46aee40b01ca4efc2536855eacead56a21
7johnsz <contatoerro708@gmail.com>
2025-03-01 16:56:42 -0300

    feat: :sparkles: add stock market transactions and buy endpoints to main application

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 # from fastapi.middleware.gzip import GZipMiddleware
 from .controllers.routers.account.finances import checking_account as account
+from .controllers.routers.account.finances.stockmarket import transactions
 from .controllers.routers.account.finances.history import withdraw_history
 from .controllers.routers.account.finances.history import deposit_history
 from .controllers.routers.account.finances.stockmarket import asset
+from .controllers.routers.account.finances.stockmarket import buy
 from .controllers.routers.account.finances import withdraw
 from .controllers.routers.account.finances import deposit
 from .controllers.routers.account import signup
@@ -14,12 +16,14 @@ app = FastAPI(docs="/docs")
 # app.add_middleware(GZipMiddleware)
 app.include_router(withdraw_history.router)
 app.include_router(deposit_history.router)
+app.include_router(transactions.router)
 app.include_router(withdraw.router)
 app.include_router(deposit.router)
 app.include_router(account.router)
 app.include_router(signup.router)
 app.include_router(login.router)
 app.include_router(asset.router)
+app.include_router(buy.router)
 
 @app.get("/")
 async def root():
