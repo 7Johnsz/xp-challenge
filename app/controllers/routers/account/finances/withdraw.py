75e67bcf380b0341914cca534bcd4a394fab4d44
7johnsz <contatoerro708@gmail.com>
2025-02-28 23:02:30 -0300

    feat: :sparkles: implement withdraw endpoint for processing withdrawals and updating history

---
+++ app/controllers/routers/account/finances/withdraw.py
@@ -0,0 +1,42 @@
+from fastapi import Request, status, Response
+from .....models.withdraw_schema import Withdraw
+from fastapi.responses import ORJSONResponse
+from .....services.auth import AuthService
+from ....config.database import redis_conn  
+from ....config.database import database
+from ....config.api import router
+from .....services.auth import find_key
+
+import datetime
+
+@router.patch("/withdraw", response_class=ORJSONResponse)
+@AuthService
+async def withdraw(request: Request, response: Response, withdraw_data: Withdraw):
+    try:
+        user_key = find_key(request.headers.get('Authorization').split()[1])[0]                
+        id_user = database.query("SELECT CodClient FROM client WHERE email = %s", (user_key,))[0][0]
+        balance = database.query("SELECT balance FROM client WHERE email = %s", (user_key,))[0][0]
+        
+        database.execute("UPDATE client SET balance = balance - %s WHERE CodClient = %s", (withdraw_data.value, id_user))
+        database.execute("INSERT INTO withdraw_history (CodClient, value) VALUES (%s, %s)", (id_user, withdraw_data.value))
+                      
+        return {
+            "status": "success",
+            "message": "Deposit processed successfully.",
+            "deposit_details": {
+                "email": user_key,
+                "amount": withdraw_data.value},
+            "account_summary": {
+                "new_balance": database.query("SELECT balance FROM client WHERE email = %s",
+                                              (user_key,))[0][0],
+                "previous_balance": balance},
+            "datetime": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+            
+    except Exception as e:
+        database.conn.rollback()
+        print(e)
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {
+            "status": "error",
+            "message": "Internal server error",
+            "datetime": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 # from fastapi.middleware.gzip import GZipMiddleware
 from .controllers.routers.account.finances import checking_account as account
-from .controllers.routers.account.finances import deposit_history
-from .controllers.routers.account.finances import withdraw_history
+from .controllers.routers.account.finances.history import withdraw_history
+from .controllers.routers.account.finances.history import deposit_history
+from .controllers.routers.account.finances import withdraw
 from .controllers.routers.account.finances import deposit
 from .controllers.routers.account import signup
 from .controllers.routers.account import login
@@ -12,6 +13,7 @@ app = FastAPI(docs="/docs")
 # app.add_middleware(GZipMiddleware)
 app.include_router(withdraw_history.router)
 app.include_router(deposit_history.router)
+app.include_router(withdraw.router)
 app.include_router(deposit.router)
 app.include_router(account.router)
 app.include_router(signup.router)
