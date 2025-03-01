fe0abc8ce54647ff6fbda35f8993f84a816effb4
7johnsz <contatoerro708@gmail.com>
2025-03-01 18:20:28 -0300

    feat: :sparkles: add admin routes for withdraw and deposit history to main application

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 # from fastapi.middleware.gzip import GZipMiddleware
 from .controllers.routers.finances import withdraw, deposit, checking_account as account
+from .controllers.routers.admin import all_withdraw_history, all_deposit_history, users
 from .controllers.routers.finances.stockmarket import asset, sell, buy, transactions
 from .controllers.routers.finances.history import withdraw_history, deposit_history
 from .controllers.routers.account import signup, login
+
 
 from fastapi import FastAPI
 
 app = FastAPI(docs="/docs")
 
 # app.add_middleware(GZipMiddleware)
+app.include_router(all_withdraw_history.router)
+app.include_router(all_deposit_history.router)
 app.include_router(withdraw_history.router)
 app.include_router(deposit_history.router)
 app.include_router(transactions.router)
@@ -16,6 +20,7 @@ app.include_router(withdraw.router)
 app.include_router(deposit.router)
 app.include_router(account.router)
 app.include_router(signup.router)
+app.include_router(users.router)
 app.include_router(login.router)
 app.include_router(asset.router)
 app.include_router(sell.router)
