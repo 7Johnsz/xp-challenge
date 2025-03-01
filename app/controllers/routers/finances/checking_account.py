7bb60cbb8ce8360cb03a4eff273cf12e129fb1ab
7johnsz <contatoerro708@gmail.com>
2025-03-01 18:43:33 -0300

    feat: :sparkles: update checking_account endpoint to include asset information and remove deposit/withdraw history

--- app/controllers/routers/finances/checking_account.py
+++ app/controllers/routers/finances/checking_account.py
@@ -17,23 +17,25 @@ async def checking_account(request: Request, response: Response):
         balance = database.query("SELECT balance FROM client WHERE email = %s", (user_key,))[0][0]
         created_at = database.query("SELECT created_at FROM client WHERE email = %s", (user_key,))[0][0]
 
-        deposit_results = database.query("SELECT * FROM deposit_history WHERE CodClient = %s", (id_user,))
-        withdraw_results = database.query("SELECT * FROM withdraw_history WHERE CodClient = %s", (id_user,))
-        
-        deposit_history = []
-        for deposit in deposit_results:
-            deposit_history.append({
-                "id": deposit[0],
-                "amount": deposit[2],
-                "transaction_at": deposit[3]
-            })
-            
-        withdraw_history = []
-        for withdraw in withdraw_results:
-            withdraw_history.append({
-                "id": withdraw[0],
-                "amount": withdraw[2],
-                "transaction_at": withdraw[3]
+        assets_results = database.query("SELECT * FROM asset_client WHERE CodClient = %s", (id_user,))
+
+        if len(assets_results) == 0:
+            return {
+                "status": "success",
+                "message": "Account details fetched successfully.",
+                "account_details": {
+                    "email": user_key,
+                    "balance": balance,
+                    "created_at": created_at
+                },
+                "datetime": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+
+        assets_info = []
+        for asset in assets_results:
+            print(asset)
+            assets_info.append({
+                "ticker": asset[0],
+                "quantity": asset[1]
             })
         
         return {
@@ -44,8 +46,7 @@ async def checking_account(request: Request, response: Response):
                 "balance": balance,
                 "created_at": created_at
             },
-            "deposit_history": deposit_history,
-            "withdraw_history": withdraw_history,
+            "assets": assets_info,
             "datetime": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
          
     except Exception as e:
