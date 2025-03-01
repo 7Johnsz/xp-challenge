5a6208101f5848bedbd9279692c9e1cad8389146
7johnsz <contatoerro708@gmail.com>
2025-02-28 23:03:00 -0300

    feat: :sparkles: add deposit and withdraw history endpoints to fetch user transaction records

--- app/controllers/routers/account/finances/history/withdraw_history.py
+++ app/controllers/routers/account/finances/history/withdraw_history.py
@@ -1,32 +1,32 @@
 from fastapi import Request, status, Response
 from fastapi.responses import ORJSONResponse
-from .....services.auth import AuthService
-from ....config.database import database
-from .....services.auth import find_key
-from ....config.api import router
+from ......services.auth import AuthService
+from .....config.database import database
+from ......services.auth import find_key
+from .....config.api import router
 
 import datetime
 
-@router.get("/deposit-history", response_class=ORJSONResponse)
+@router.get("/withdraw-history", response_class=ORJSONResponse)
 @AuthService
-async def deposit_history(request: Request, response: Response):
+async def withdraw_history(request: Request, response: Response):
     try:
         user_key = find_key(request.headers.get('Authorization').split()[1])[0]
         id_user = database.query("SELECT CodClient FROM client WHERE email = %s", (user_key,))[0][0]
-        deposit_results = database.query("SELECT * FROM deposit_history WHERE CodClient = %s", (id_user,))
+        withdraw_results = database.query("SELECT * FROM withdraw_history WHERE CodClient = %s", (id_user,))
         
-        deposit_history = []
-        for deposit in deposit_results:
-            deposit_history.append({
-                "id": deposit[0],
-                "amount": deposit[2],
-                "transaction_at": deposit[3]})
+        withdraw_results = []
+        for withdraw in withdraw_results:
+            withdraw_results.append({
+                "id": withdraw[0],
+                "amount": withdraw[2],
+                "transaction_at": withdraw[3]})
         
         return {
             "status": "success",
-            "message": "Deposit history fetched successfully.",
+            "message": "Withdraw history fetched successfully.",
             "email": user_key,
-            "deposit_history": deposit_history,
+            "withdraw_history": withdraw_results,
             "datetime": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
          
     except Exception as e:
