d06cd41970ce0ea2abf49a2bccba919b1bd1119f
7johnsz <contatoerro708@gmail.com>
2025-03-04 18:32:40 -0300

    fix: :bug: improve Authorization header check and handle Redis errors in AuthService decorator

--- app/services/auth.py
+++ app/services/auth.py
@@ -29,38 +29,42 @@ def check_redis_value(value: str):
 def AuthService(func):
     """
     A decorator to ensure that the request has the correct Authorization header.
-    This decorator checks if the Authorization header in the request matches the expected value.
-    If it does, it proceeds to execute the wrapped function. Otherwise, it raises an HTTPException with a status code of 401.
-
-    Returns:
-    Callable: The wrapped function with Authorization header check.
-
-    Raises:
-    HTTPException: If the Authorization header does not match the expected value.
     """
     @wraps(func)
     async def wrapper(request: Request, *args, **kwargs):
         auth_header = request.headers.get('Authorization')
         now = datetime.now().isoformat()
 
-        if auth_header == None:
+        if auth_header is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail={
                     "msg": "You don't have permission to access this page",
                     "timestamp": now
-                })
-            
-        auth_result = auth_header.split()[1]
-        auth_response = check_redis_value(auth_result)
+                }
+            )
 
-        if auth_response[0] == False:
+        try:
+            auth_result = auth_header.split()[1]
+            auth_response = check_redis_value(auth_result)
+
+            if not auth_response[0]:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail={
+                        "msg": "You don't have permission to access this page",
+                        "timestamp": now
+                    }
+                )
+
+        except Exception as e:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail={
-                    "msg": "You don't have permission to access this page",
+                    "msg": f"Redis error: {str(e)}",
                     "timestamp": now
-                })
-            
+                }
+            )
+
         return await func(request, *args, **kwargs)
     return wrapper
